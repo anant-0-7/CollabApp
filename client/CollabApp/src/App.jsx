5a829cc1b6e4b77a27456c51188210831dc4ab14
Anant Teltia <anantteltia@gmail.com>
2024-08-24 23:06:39 +0530

    All projects added

--- client/CollabApp/src/App.jsx
+++ client/CollabApp/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Signup from './components/Signup';
+import All from "./components/All";
 
 function App () {
   return (
@@ -14,7 +15,7 @@ function App () {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/all" element={<Navbar />} />
+        <Route path="/all" element={<All />} />
       </Routes>
     </BrowserRouter>
   );
--- client/CollabApp/src/components/Navbar.jsx
+++ client/CollabApp/src/components/Navbar.jsx
@@ -1,44 +1,31 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import "../../public/Navbar.css";
 
-
-function NavigationBar(){
-
-    return (<header>
-
-    
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">CollabApp</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
-        
-
+function NavigationBar() {
+  return (
+    <header>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container className="navbar-container">
+          <Navbar.Brand href="#home">CollabApp</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">All Projects</Nav.Link>
+              <Nav.Link href="#link">My Projects</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto"> {/* Aligns the new option to the right */}
+              <Button variant="outline-light" href="#create-project">
+                Create New Project
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-
-    )
-
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
